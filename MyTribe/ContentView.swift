85a6580614385627f4ac00adf94c7271a50d97af
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-03-21 17:20:39 -0500

    Initial Commit

---
+++ MyTribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTribe
+//
+//  Created by J. DeWeese on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTribe/MyTribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTribeApp.swift
+//  MyTribe
+//
+//  Created by J. DeWeese on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
